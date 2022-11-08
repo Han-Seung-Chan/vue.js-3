9b4c18f8995f39635d2331dfacb2bb4cef4233b1
khan <tmdcks63@naver.com>
2022-11-08 18:46:39 +0900

    feat : 게시글 목데이터

---
+++ src/data/posts.js
@@ -0,0 +1,11 @@
+const posts = [
+  { id: 1, title: '제목 1', content: '내용 1', createAt: '2022-01-01' },
+  { id: 2, title: '제목 2', content: '내용 2', createAt: '2022-02-02' },
+  { id: 3, title: '제목 3', content: '내용 3', createAt: '2022-03-03' },
+  { id: 4, title: '제목 4', content: '내용 4', createAt: '2022-04-04' },
+  { id: 5, title: '제목 5', content: '내용 5', createAt: '2022-05-05' },
+];
+
+export const getPosts = () => {
+  return posts;
+};
