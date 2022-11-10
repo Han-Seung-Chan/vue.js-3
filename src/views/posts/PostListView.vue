bd6ffa495125fcba31d3829b7510dba9bd99e28e
khan <tmdcks63@naver.com>
2022-11-10 18:33:19 +0900

    feat : pagination 구현

--- src/views/posts/PostListView.vue
+++ src/views/posts/PostListView.vue
@@ -12,9 +12,32 @@
         ></PostItem>
       </div>
     </div>
-    <hr class="my-4" />
+    <nav class="mt-5" aria-label="Page navigation example">
+      <ul class="pagination justify-content-center">
+        <li class="page-item">
+          <a class="page-link" href="#" aria-label="Previous">
+            <span aria-hidden="true">&laquo;</span>
+          </a>
+        </li>
+        <li
+          v-for="page in pageCount"
+          :key="page"
+          class="page-item"
+          :class="{ active: params._page === page }"
+        >
+          <a class="page-link" href="#" @click.prevent="params._page = page">{{ page }}</a>
+        </li>
+
+        <li class="page-item">
+          <a class="page-link" href="#">
+            <span aria-hidden="true">&raquo;</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <hr class="my-5" />
     <AppCard>
-      <PostDetailView :id="'1'" />
+      <PostDetailView :id="parseInt(1)" />
     </AppCard>
   </div>
 </template>
@@ -23,14 +46,30 @@
 import PostItem from '@/components/posts/PostItem.vue';
 import AppCard from '@/components/AppCard.vue';
 import PostDetailView from '@/views/posts/PostDetailView.vue';
-import { ref } from 'vue';
-import { getPosts } from '@/data/posts';
+import { computed, ref, watchEffect } from 'vue';
 import { useRouter } from 'vue-router';
+import { getPosts } from '@/api/posts';
+
+const params = ref({
+  _sort: 'createAt',
+  _order: 'desc',
+  _page: 1,
+  _limit: 3,
+});
+const totalCount = ref(0);
+const pageCount = computed(() => Math.ceil(totalCount.value / params.value._limit));
 
 const posts = ref([]);
-(() => {
-  posts.value = getPosts();
-})();
+const fetchPosts = async () => {
+  try {
+    const { data, headers } = await getPosts(params.value);
+    posts.value = data;
+    totalCount.value = headers['x-total-count'];
+  } catch (err) {
+    console.error(err);
+  }
+};
+watchEffect(fetchPosts);
 
 const router = useRouter();
 const goDetailPage = (id) => router.push(`/posts/${id}`);
