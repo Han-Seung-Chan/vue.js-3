e14a4a0aa5d27691b18cb3bb2cc10cfd60b72278
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2022-11-10 23:38:30 +0900

    style : 불필요한 코드 제거

--- src/layout/TheHeader.vue
+++ src/layout/TheHeader.vue
@@ -9,7 +9,6 @@
         data-bs-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent"
         aria-expanded="false"
-        aria-label="Toggle navigation"
       >
         <span class="navbar-toggler-icon"></span>
       </button>
