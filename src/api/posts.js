c77354d14348b5aa65cd7f56df6652021ba3408c
khan <tmdcks63@naver.com>
2022-11-11 13:18:00 +0900

    refactor : 요청 url template 변경

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,8 +1,8 @@
 import { posts } from '.';
 
-export const getPostsId = (id) => posts.get(id);
+export const getPostsId = (id) => posts.get(`/${id}`);
 
-export const getPosts = (params) => posts.get('', { params });
+export const getPosts = (params) => posts.get(`/`, { params });
 export const createPost = (data) => posts.post('', data);
-export const updatePost = (id, data) => posts.put(id, data);
-export const deletePost = (id) => posts.delete(id);
+export const updatePost = (id, data) => posts.put(`/${id}`, data);
+export const deletePost = (id) => posts.delete(`/${id}`);
